23da5828b32e9be2dcf122e2c845736e9950a59b
Devin Costello <86488145+DevinCostello@users.noreply.github.com>
2023-01-13 14:52:49 -0500

    bit more ui work

--- frontend/src/components/CartItem.jsx
+++ frontend/src/components/CartItem.jsx
@@ -12,23 +12,24 @@ const CartItem = ({ item }) => {
 
     return (
         <main key={item._id} className={styles.item}>
+            <img src="https://via.placeholder.com/175x225/" alt="" />
             <section>
-                <h2>{item.name}</h2>
-                <h3>{item.category}</h3>
-                <h3>Color: {item.color}</h3>
-                <h3>Size: {item.size}</h3>
+                <h3>${currency(item.price).multiply(item.quantity).value}</h3>
+                <h3>{item.name}</h3>
+                <h4>{item.category}</h4>
+                <h4>{item.color}</h4>
+                <h4>{item.size}</h4>
             </section>
-            <section className={styles.item_right}>
+            
+            {/* <section className={styles.item_right}>
                 <form className={styles.form} onSubmit={e => e.preventDefault()}>
                     <input type="text" placeholder={item.quantity} />
                     <button type="submit" onClick={(e) => updateCart({ id: item._id, quantity: parseInt(e.target.form[0].value) })}>Update</button>
                     <button onClick={() => DeleteItem(item)}>Remove</button>
-
                     {status === "rejected" && <p className={styles.error}>{updateError.data.message}</p>}
-
                 </form>
-                <h3>${currency(item.price).multiply(item.quantity).value}</h3>
-            </section>
+            </section> */}
+
         </main>)
 
 }
